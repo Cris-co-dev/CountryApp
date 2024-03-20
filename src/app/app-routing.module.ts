cee43f5dfaf070633e3ae203743c723d246be8f1
Cris <corsamps@gmail.com>
2024-03-20 15:20:38 -0500

    Add countries module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./shared/pages/home-page/home-page.component";
 import {AboutPageComponent} from "./shared/pages/about-page/about-page.component";
 import {ContactPageComponent} from "./shared/pages/contact-page/contact-page.component";
+import {ByCapitalPageComponent} from "./countries/pages/by-capital-page/by-capital-page.component";
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomePageComponent
+  // },
   {
-    path: '',
-    component: HomePageComponent
-  },{
   path: 'about',
   component: AboutPageComponent
   },
   {
     path: 'contact',
     component: ContactPageComponent
-  },{
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(module => module.CountriesModule),
+  },
+  {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'countries'
   },
 ];
 
